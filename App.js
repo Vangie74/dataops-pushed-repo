24c5c78c5f7df3be8f065d0ade8f8ad8bf849b6b
Vangie74 <93350323+Vangie74@users.noreply.github.com>
2021-11-14 12:17:12 +0000

    Add files via upload
    Assignment React App

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
+import logo1 from './logo1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo1" />
+	<img src={logo} className="App-logo" alt="logo" />
         <p>
-          I changed the message!
+          INFINITE POSSIBILITIES!!
         </p>
         <a
           className="App-link"
